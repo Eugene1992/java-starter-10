c66b0c3d79239a6cff41b927e5d8d9ca719f6654
Eugene1992 <deyneko55@gmail.com>
2016-08-04 10:47:43 +0300

    Четные и нечетные числа 8.4.16

---
+++ sergey/src/main/java/lesson8416/lesson.java
@@ -0,0 +1,26 @@
+package lesson8416;
+
+/**
+ * Created by Student on 8/4/2016.
+ */
+public class lesson {
+    public static void main(String[] args) {
+        int number = 25;
+        int a = number % 2;
+        if((number - a) == number){
+            System.out.println("Ваше число четное");
+        } else{
+            System.out.println("Ваше число нечетное");
+        }
+        if(number == 0){
+            System.out.println("Ноль не относится к четным и нечетным");
+        }
+
+
+
+
+
+
+
+        }
+    }
