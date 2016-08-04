3c2a55ca1d265b36a1ea61c5a930b86e9c9c2c57
Vasiliy <Vasiliy.Kuguk@gmail.com>
2016-08-04 09:23:35 +0300

    Test commit.

--- vasiliy/src/main/java/Test.java
+++ vasiliy/src/main/java/Test.java
@@ -2,5 +2,7 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
     }
 }
