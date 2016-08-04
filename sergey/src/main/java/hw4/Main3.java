56d56afc2f0fc5a4fdcc549dae85df8ba8d29f4e
BOSSJNR <sergei_planes@ukr.net>
2016-08-04 17:08:28 +0300

    Второе задание уже готово

---
+++ sergey/src/main/java/hw4/Main3.java
@@ -0,0 +1,22 @@
+package hw4;
+/*  В три переменные a, b и c записаны три вещественных числа. Создать программу, которая
+    будет находить и выводить на экран вещественные корни квадратного уравнения ax²+bx+c=0,
+            либо сообщать, что корней нет.*/
+public class Main3 {
+    public static void main(String[] args) {
+        int a = 12;
+        int b = 10;
+        int c = 13;
+        int D;
+        double result1;
+        double result2;
+        D = b * b - 4 * a * c;
+        result1 = (-b + Math.sqrt(D)) / (2 * a);
+        result2 = (b + Math.sqrt(D)) / (2 * a);
+   if(D < 0){
+       System.out.print("Данное квадратное уравнение не имеет корней.");
+
+   }
+        System.out.println(result1 + "," + result2);
+    }
+}
