0e14ee676e6fbdf536ad1ff0f646d616207f9bfa
Eugene1992 <deyneko55@gmail.com>
2016-08-05 10:56:05 +0300

    Четные и нечетные числа 8.4.16

--- sergey/src/main/java/hw03/Main.java
+++ sergey/src/main/java/hw03/Main.java
@@ -2,15 +2,18 @@ package hw03;
 public class Main {
     public static void main(String args[]) {
         int c = 123;
-        int r1 = c / 2;
-        int r2 = r1 / 2;
-        int r3 = r2 / 2;
-        int r4 = r3 / 2;
-        int r5 = r4 / 2;
-        int r6 = r5 / 2;
+        int c1 = 1;
+        int result = 0;
+        String res = "";
+        while(c > 0){
+            c1 = c % 2;
 
+            res = c1 + res;
+            c = c / 2;
+        }
+        System.out.println(res);
 
-        int f1 = r1 * 2;
+     /*   int f1 = r1 * 2;
         int f2 = r2 * 2;
         int f3 = r3 * 2;
         int f4 = r4 * 2;
@@ -25,5 +28,5 @@ public class Main {
         int number5 = r4 - f5;
         int number6 = r5 - f6;
         System.out.print( 1 + "" + number6 + "" + number5 + "" +  number4 + "" +  number3 + "" +  number2 + "" +  number1 );
-    }
+    */}
 }
