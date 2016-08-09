3c3cc761b747e9a8ea66fb62c5fb754bb2c97ca9
Vasiliy <Vasiliy.Kuguk@gmail.com>
2016-08-09 13:22:05 +0300

    Lesson7

--- vasiliy/src/main/java/lesson7/Lesson7_2.java
+++ vasiliy/src/main/java/lesson7/Lesson7_2.java
@@ -7,34 +7,35 @@ public class Lesson7_2 {
     public static void main(String[] args) {
         System.out.println(get13());
     }
+
     static int get13() {
         int a = 131133;
         int b = 0;
         int c = 0;
         int counter = 0;
-        b = a%1000000;
-        c = b/10000;
-        if (c==13){
+        b = a % 1000000;
+        c = b / 10000;
+        if (c == 13) {
             counter++;
         }
-        b = a%100000;
-        c = b/1000;
-        if (c==13){
+        b = a % 100000;
+        c = b / 1000;
+        if (c == 13) {
             counter++;
         }
-        b = a%10000;
-        c = b/100;
-        if (c==13){
+        b = a % 10000;
+        c = b / 100;
+        if (c == 13) {
             counter++;
         }
-        b = a%1000;
-        c = b/10;
-        if (c==13){
+        b = a % 1000;
+        c = b / 10;
+        if (c == 13) {
             counter++;
         }
-        b = a%100;
-        c = b/1;
-        if (c==13){
+        b = a % 100;
+        c = b / 1;
+        if (c == 13) {
             counter++;
         }
         return counter;
