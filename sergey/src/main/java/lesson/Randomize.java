6fb31e2fb3f741aaecdc0191341eef710092242d
BOSSJNR <sergei_planes@ukr.net>
2016-08-09 20:52:44 +0300

    Второе задание уже готово

--- sergey/src/main/java/lesson/Randomize.java
+++ sergey/src/main/java/lesson/Randomize.java
@@ -1,21 +1,69 @@
 package lesson;
+
 public class Randomize {
     public static void main(String args[]) {
         /*getRandom();*/
+        get13From999999();
     }
 
-    static int get13From999999(){
-        int full = 999999;
+    static int get13From999999() {
+        int full = 0;
+        int full1 = 0;
+        int full2 = 0;
+        int full3 = 0;
+        int full4 = 0;
+        int full5 = 0;
+        int full6 = 0;
         int count = 0;
-        for (int i = 0; i < 999999; i++) {
-        full = full / 100000 % 10;
-        full = full / 10000 % 10;
-        full = full / 1000 % 10;
-        full = full / 100 % 10;
-        full = full / 10% 10;
-        return count;
+        int over = 0;
+        for (int i = 0; i < 999999; ) {
+            full++;
+            full1 = full / 100000 % 10;
+            full2 = full / 10000 % 10;
+            full3 = full / 1000 % 10;
+            full4 = full / 100 % 10;
+            full5 = full / 10 % 10;
+            full6 = full % 10;
+            if (full1 == 1) {
+                if (full2 == 3) {
+                    ++over;
+                }
+            }
+            if (full2 == 1) {
+                if (full3 == 3) {
+                    ++over;
+                }
+            }
+            if (full3 == 1) {
+                if (full4 == 3) {
+                    ++over;
+                }
+                if (full3 == 1) {
+                    if (full4 == 3) {
+                        ++over;
+                    }
+                    if (full4 == 1) {
+                        if (full5 == 3) {
+                            ++over;
+                        }
+                    }
+                    if (full5 == 1) {
+                        if (full6 == 3) {
+                            ++over;
+                        }
+                    }
+
+                    System.out.print(over);
+                }
+            }
+        }
+        return over;
     }
+
 }
+
+
+
     /*static double getRandom(){
 
         double eagleside = 0;
