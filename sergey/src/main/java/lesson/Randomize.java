5d925991c0abcb627dc389dde4359939d51fc54c
Eugene1992 <deyneko55@gmail.com>
2016-08-11 11:11:39 +0300

    Четные и нечетные числа 8.4.16

--- sergey/src/main/java/lesson/Randomize.java
+++ sergey/src/main/java/lesson/Randomize.java
@@ -1,94 +1,28 @@
 package lesson;
+
 public class Randomize {
     public static void main(String args[]) {
         /*getRandom();*/
-        get13From999999();
+        System.out.println("    " + getPassword(1));
+        System.out.println("   " +getPassword(3));
+        System.out.println("  " +getPassword(5));
+        System.out.println(" " +getPassword(7));
+        System.out.println(getPassword(9));
     }
 
-    static int get13From999999() {
-        int full = 133713;
-        int full1 = 0;
-        int full2 = 0;
-        int full3 = 0;
-        int full4 = 0;
-        int full5 = 0;
-        int full6 = 0;
-        int count = 0;
-        int over = 0;
-        full1 = full / 100000 % 10;
-        full2 = full / 10000 % 10;
-        full3 = full / 1000 % 10;
-        full4 = full / 100 % 10;
-        full5 = full / 10 % 10;
-        full6 = full % 10;
-            if (full1 == 1) {
-                if (full2 == 3) {
-                    ++over;
-                }
+    static String getPassword(int length) {
+        String pass = "";
+        for (int i = 0; i < length; ) {
+            int key = (int) (Math.random() * 74 + 48);
+            if (key > 57 && key < 65 || key > 90 && key < 97) {
+                continue;
             }
-            if (full2 == 1) {
-                if (full3 == 3) {
-                    ++over;
-                }
-            }
-            if (full3 == 1) {
-                if (full4 == 3) {
-                    ++over;
-                }
-                if (full4 == 1) {
-                    if (full5 == 3) {
-                        ++over;
-                    }
-                    if (full5 == 1) {
-                        if (full6 == 3) {
-                            ++over;
-                        }
-                    }
-                    }
-
-                    System.out.print(full1);
-                    System.out.print(full2);
-                    System.out.print(full3);
-                    System.out.print(full4);
-                    System.out.print(full5);
-                    System.out.println(full6);
-                    System.out.print(over);
-                }
-        return over;
+            char letter = (char) key;
+            pass += letter;
+            ++i;
+        }
+        return pass;
     }
 
 }
-
-
-
-    /*static double getRandom(){
-
-        double eagleside = 0;
-        double reshkaside = 0;
-        double proc = 0;
-        int over = 0;
-        for(int i = 0; i < 100; i++){
-            eagleside = 0;
-            reshkaside = 0;
-            for (int j = 0; j < 100; j++) {
-                int random = (int) (Math.random() * 2);
-                if (random == 0) {
-                    eagleside++;
-                }
-                if (random == 1) {
-                    reshkaside++;
-                }
-            }
-            if(reshkaside - eagleside > 10){
-                ++over;
-            }
-            if(eagleside - reshkaside > 10){
-                ++over;
-            }
-        }
-        System.out.println("Отклонениеот нормы " + over + " раз");
-        System.out.println(reshkaside + " Выпала решка");
-        System.out.println(eagleside + " Выпал орел");
-        return proc;
-    }*/
 
